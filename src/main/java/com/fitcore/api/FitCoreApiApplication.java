09e72653acf3a302407b59bfe20a7b1ad5af1a6b
dbrud <sw_rnaster@kakao.com>
2026-04-05 18:22:48 +0900

    init

---
+++ src/main/java/com/fitcore/api/FitCoreApiApplication.java
@@ -0,0 +1,1 @@
+package com.fitcore.api;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;@SpringBootApplicationpublic class FitCoreApiApplication {    public static void main(String[] args) {        SpringApplication.run(FitCoreApiApplication.class, args);    }}
---
+++ src/test/java/com/fitcore/api/FitCoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitcore.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitCoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
